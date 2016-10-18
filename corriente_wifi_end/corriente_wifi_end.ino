1a097cc82e01f19959fde3844cfafac19a378632
Carlos Benavides <cebenavides@uninorte.edu.co>
2016-10-18 11:09:17 -0500

    Corregido el archivo de Arduino

--- corriente_wifi_end/corriente_wifi_end.ino
+++ corriente_wifi_end/corriente_wifi_end.ino
@@ -39,21 +39,34 @@
 
 #include <SoftwareSerial.h>
 int voltaje_pin=0, corriente_pin=1;
-double valor_corriente[310],valor_voltaje[310];
+double valor_corriente[110],valor_voltaje[110];
 int i=0, j=0, tiempo;
 double a,b,pot;
+ SoftwareSerial ModuloWiFi(3,2); // RX, TX
+
+void GETrequest(double i){
+  ModuloWiFi.write("AT+CIPSTART=\"TCP\",\"192.168.1.3\",80\r\n");
+  delay(10);
+  int longi = String(i).length()+56;
+  ModuloWiFi.print("AT+CIPSEND=");
+  ModuloWiFi.print(longi);
+  ModuloWiFi.print("\r\n");
+  delay(10);
+  ModuloWiFi.print("GET /realtime/add?value=");
+  ModuloWiFi.print(i);
+  ModuloWiFi.print(" HTTP/1.1\r\nHost: 192.168.1.3\r\n\r\n");
+}
+
 void setup(){
  Serial.begin(115200);
- //------------------------------------------------
-
-   
+ ModuloWiFi.begin(115200);   
 }
 
 void loop(){
   
    i=0;
    tiempo = millis();
-   while(i< 300 ){
+   while(i< 100 ){
      valor_corriente[i] = analogRead(corriente_pin);
      delay(1);
      valor_voltaje[i] = analogRead(voltaje_pin);
@@ -64,9 +77,9 @@ void loop(){
    j=20;
    pot=0;
    while(j<=i-4){
-    a=(((534 - valor_corriente[j])*5/1023.0)/0.1);
+    a=(((512 - valor_corriente[j])*5/1023.0)/0.1);
     b=(((513 - valor_voltaje[j])*5/1023.0));
-    b=(b-1.444)*143;
+    b=(b-1.024)*143;
     a=a*1.45;
     //Serial.println("dayan es puta");
     //Serial.println(a,4);
@@ -77,9 +90,13 @@ void loop(){
    }
        //Serial.println(j);
     //Serial.println(i);
+    if (pot<0) {
+      pot=0.0;
+      }
     pot=pot/(j-20);
    Serial.println(pot,4);
- double  valor_corriente[310]={0};
- double  valor_voltaje[310]={0};
+   GETrequest(pot);
+ double  valor_corriente[110]={0};
+ double  valor_voltaje[110]={0};
    delay(2000);
 }
